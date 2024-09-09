248ed1778914e4c58f0103fbf8277a0887209612
Oscar Roberts <roberts_oscar@hotmail.com>
2024-09-09 09:54:12 -0300

    se agregan comentarios

--- conversion-dinero.py
+++ conversion-dinero.py
@@ -1,9 +1,9 @@
 
-tipo_de_cambio_eur_a_mex = 23.7
+tipo_de_cambio_eur_a_mex = 23.7 # en un caso real se toma el dato de una base de datos 
 tipo_de_cambio_usd_a_mex = 20.75
 
 # 1. Solicitar el tipo de moneda y convertir a mayúsculas 
-tipo_de_moneda = input("Por favor, ingresá la moneda que querés convertir (Euro / Dolar): ").upper()
+tipo_de_moneda = input("Por favor, ingresá la moneda que querés convertir (EURO / USD): ").upper()
 
 # 2. Solicitar el monto y reemplazar coma por punto si es necesario
 monto_a_convertir = input("Por favor, ingrese el monto a convertir: ").replace(',', '.')
@@ -15,7 +15,7 @@ monto_a_convertir = float(monto_a_convertir)
 if tipo_de_moneda == "EURO":
     resultado = monto_a_convertir * tipo_de_cambio_eur_a_mex
     print(f"El monto en pesos mexicanos es: {resultado:.2f}") # sin :.2f el resultado se imprimiría con todos los decimales
-elif tipo_de_moneda == "DOLAR":
+elif tipo_de_moneda == "USD":
     resultado = monto_a_convertir * tipo_de_cambio_usd_a_mex
     print(f"El monto en pesos mexicanos es: {resultado:.2f}")
 else:
